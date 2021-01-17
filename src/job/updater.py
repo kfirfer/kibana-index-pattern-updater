b8f440f23668d04dfdeb62a0c11d8bb5bb5a9c44
Kfir <kfirfer@gmail.com>
2021-01-17 11:50:42 +0200

    wip

--- src/job/updater.py
+++ src/job/updater.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import requests
 import base64
 
-from src import KUBERNETES_HOST, KUBERNETES_TOKEN, KIBANA_HOST, KIBANA_USERNAME, KIBANA_PASSWORD
+import requests
+from kubernetes import client, config
+
+from src import KIBANA_HOST, KIBANA_USERNAME, KIBANA_PASSWORD, LOAD_KUBECONFIG, LOAD_INCLUSTER_CONFIG
 
 KIBANA_AUTH = "{}:{}".format(KIBANA_USERNAME, KIBANA_PASSWORD)
 message_bytes = KIBANA_AUTH.encode('ascii')
@@ -11,12 +13,13 @@ KIBANA_AUTH = base64_bytes.decode('ascii')
 
 
 def get_namespaces():
-    url = "{}/api/v1/namespaces".format(KUBERNETES_HOST)
-    headers = {
-        'authorization': 'Bearer {}'.format(KUBERNETES_TOKEN)
-    }
-    response = requests.request("GET", url, headers=headers, verify=False, timeout=60).json()
-    return response
+    if LOAD_KUBECONFIG == "1":
+        config.load_kube_config()
+    if LOAD_INCLUSTER_CONFIG == "1":
+        config.load_incluster_config()
+    v1 = client.CoreV1Api()
+    namespaces = v1.list_namespace(watch=False)
+    return namespaces
 
 
 def create_index_patterns(namespace):
@@ -51,8 +54,8 @@ def is_index_pattern_exists(namespace):
 
 def job():
     namespaces = get_namespaces()
-    for item in namespaces["items"]:
-        name = item["metadata"]["name"]
+    for item in namespaces.items:
+        name = item.metadata.name
         index_pattern_exists = is_index_pattern_exists(name)
         if index_pattern_exists is False:
             create_index_patterns(name)
